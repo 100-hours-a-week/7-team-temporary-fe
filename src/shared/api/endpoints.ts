f3035b9655bcef585e863ea14da9469376291a90
happy7yong <happy7yong83@gmail.com>
2026-01-20 11:17:54 +0900

    feat: 로그인 엔드포인트 추가

--- src/shared/api/endpoints.ts
+++ src/shared/api/endpoints.ts
@@ -2,6 +2,10 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "";
 
 export const path = (endpoint: string) => `${API_BASE_URL}${endpoint}`;
 
-export const Endpoint = {} as const;
+export const Endpoint = {
+  AUTH: {
+    LOGIN: path("/token"),
+  },
+} as const;
 
 export { API_BASE_URL };
