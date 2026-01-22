62d1b8c28792acde34e52628f25adece4d2462bc
happy7yong <happy7yong83@gmail.com>
2026-01-22 11:42:34 +0900

    chore(config): 빌드/도구 설정 갱신

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,24 @@ const nextConfig: NextConfig = {
   compiler: {
     emotion: true,
   },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
