f8a8a57f7634ad949eb0399548d278da83f3abdd
happy7yong <happy7yong83@gmail.com>
2026-01-22 11:34:20 +0900

    feat(pages-auth-login): 로그인 페이지 업데이트

--- src/pages/auth/login/ui/LoginPage.tsx
+++ src/pages/auth/login/ui/LoginPage.tsx
@@ -1,9 +1,25 @@
+"use client";
+
 import { LoginFormContainer } from "@/features/auth/login/ui";
+import { SignUpIntroPage } from "@/pages/auth/sign-up/intro";
+import { Icon, useStackPage } from "@/shared/ui";
 
 export function LoginPage() {
+  const { push } = useStackPage();
+
   return (
-    <main className="flex min-h-screen w-full flex-col items-center justify-center p-8 text-left">
-      <LoginFormContainer />
-    </main>
+    <>
+      <div>
+        <div className="text-xl font-bold text-neutral-500">
+          몰입이 시작되는 <br />
+          가장 작은 단위
+        </div>
+        <Icon
+          name="logoDefault"
+          className="h-auto w-full max-w-[200px] shrink"
+        />
+      </div>
+      <LoginFormContainer onGoToSignUp={() => push(<SignUpIntroPage />)} />
+    </>
   );
 }
