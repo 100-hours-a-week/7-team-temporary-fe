57e43cb5475704a46081890fb54210a478f168c9
happy7yong <happy7yong83@gmail.com>
2026-01-22 11:35:34 +0900

    feat(shared-api): API 엔드포인트 갱신

--- src/shared/api/endpoints.ts
+++ src/shared/api/endpoints.ts
@@ -1,11 +1,26 @@
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "";
 
-export const path = (endpoint: string) => `${API_BASE_URL}${endpoint}`;
+/**
+ * base url + endpoint path 결합 유틸
+ */
+const path = (endpoint: string): string => `${API_BASE_URL}${endpoint}`;
 
 export const Endpoint = {
-  AUTH: {
-    LOGIN: path("/token"),
+  TOKEN: {
+    BASE: path("/token"),
+  },
+  USER: {
+    BASE: path("/users"),
+    CHECK: {
+      NICKNAME: (nickname: string) => path(`/users?nickname=${encodeURIComponent(nickname)}`),
+
+      EMAIL: (email: string) => path(`/users?email=${encodeURIComponent(email)}`),
+    },
+    NICKNAME: path("/users/nickname"),
+    PASSWORD: path("/users/password"),
+  },
+  UPLOAD: {
+    PRESIGNED_URL: path("/images"),
+    UPLOAD: (imageKey: string) => path(`/images/${imageKey}`),
   },
 } as const;
-
-export { API_BASE_URL };
