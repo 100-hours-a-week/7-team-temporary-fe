27ce4abf5a255c2108f5571af52813c4a598bc53
happy7yong <happy7yong83@gmail.com>
2026-01-22 11:47:52 +0900

    feat(shared-form): 날짜/시간 입력 컴포넌트 추가

--- src/shared/form/ui/BirthDateInput.tsx
+++ src/shared/form/ui/BirthDateInput.tsx
@@ -6,16 +6,19 @@ interface BirthDateInputProps {
   register: UseFormRegisterReturn;
   placeholder?: string;
   isDisabled?: boolean;
+  invalid?: boolean;
 }
 
 export function BirthDateInput({
   register,
   placeholder = "YYYY.MM.DD",
   isDisabled,
+  invalid,
 }: BirthDateInputProps) {
   return (
     <BaseInput
       disabled={isDisabled}
+      invalid={invalid}
       placeholder={placeholder}
       register={register}
       type="text"
