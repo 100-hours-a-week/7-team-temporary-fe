a35bb06c6a32261e76d408fea07e471cf2dc49ef
happy7yong <happy7yong83@gmail.com>
2026-01-23 20:54:37 +0900

    fix(sign-up): SplitText 적용

--- src/pages/auth/sign-up/ui/steps/SleepTimeStep.tsx
+++ src/pages/auth/sign-up/ui/steps/SleepTimeStep.tsx
@@ -1,19 +1,33 @@
 "use client";
 
+import { SplitText } from "@/shared/ui";
+
 import { SleepTimePicker } from "@/features/auth/sign-up/ui";
 import { OnboardingQuestionLayout } from "@/widgets/auth/onboarding/ui";
 
 export function SleepTimeStep() {
+  const titleText = "하루 마무리 시간을 알려주세요!";
+  const handleAnimationComplete = () => {};
   return (
     <OnboardingQuestionLayout
       title={
-        <>
-          <span className="flex w-full max-w-[200px] flex-wrap">
-            하루 마무리 시간을 알려주세요!
-          </span>
-        </>
+        <SplitText
+          text={titleText}
+          delay={30}
+          duration={1.25}
+          ease="power3.out"
+          splitType="chars"
+          from={{ opacity: 0, y: 40 }}
+          to={{ opacity: 1, y: 0 }}
+          threshold={0.1}
+          rootMargin="-100px"
+          textAlign="left"
+          tag="span"
+          onLetterAnimationComplete={handleAnimationComplete}
+        />
       }
       description="수면 시간을 설정해주세요."
+      contentClassName="gap-[37px] pt-[93px] pb-[93px]"
     >
       <SleepTimePicker />
     </OnboardingQuestionLayout>
--- src/pages/auth/sign-up/ui/steps/StartStep.tsx
+++ src/pages/auth/sign-up/ui/steps/StartStep.tsx
@@ -1,15 +1,25 @@
-import { WordRevealText } from "@/shared/ui";
+import { SplitText } from "@/shared/ui";
 import { AnimatedStar } from "./AnimatedStar";
 
 export function StartStep() {
   const lines = ["반가워요!", "더 나은 하루를 만들어볼까요?"];
 
   return (
-    <section className="flex flex-col items-center justify-center gap-6 text-center">
+    <section className="text-ink-800 mx-auto flex h-full max-w-[290px] flex-col items-center justify-center gap-6 text-center">
       <AnimatedStar />
-      <WordRevealText
-        lines={lines}
-        className="text-secondary-100 text-2xl font-bold"
+      <SplitText
+        text={lines.join(" ")}
+        className="text-center text-2xl font-semibold mb-12"
+        delay={30}
+        duration={1.25}
+        ease="power3.out"
+        splitType="chars"
+        from={{ opacity: 0, y: 40 }}
+        to={{ opacity: 1, y: 0 }}
+        threshold={0.1}
+        rootMargin="-100px"
+        textAlign="center"
+        onLetterAnimationComplete={() => {}}
       />
     </section>
   );
