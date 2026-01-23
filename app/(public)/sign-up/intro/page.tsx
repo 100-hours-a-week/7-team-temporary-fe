c3ccc148887e8d9a8e0126cb77124f146c34aeb3
happy7yong <happy7yong83@gmail.com>
2026-01-23 21:59:54 +0900

    fix(build): 모듈 import & export 수정

--- app/(public)/sign-up/intro/page.tsx
+++ app/(public)/sign-up/intro/page.tsx
@@ -1,4 +1,4 @@
-import { SignUpIntroPage } from "@/pages/auth/sign-up/intro";
+import { SignUpIntroPage } from "@/pages/auth/sign-up";
 
 export default function Page() {
   return <SignUpIntroPage />;
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -2,5 +2,4 @@ export * from "./shadcn";
 export * from "@/widgets/stack";
 export * from "./icon";
 export * from "./button";
-export * from "./text";
 export { default as SplitText } from "./SplitText";
