d2975008818453831789a68b57c6b7ccbb8e1884
happy7yong <happy7yong83@gmail.com>
2026-01-23 21:41:06 +0900

    chore(config): 의존성/빌드 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,36 +10,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        red: {
-          //alpha-value : 투명도 값
-          700: "rgb(var(--color-red-700) / <alpha-value>)",
-          600: "rgb(var(--color-red-600) / <alpha-value>)",
-          500: "rgb(var(--color-red-500) / <alpha-value>)",
-          400: "rgb(var(--color-red-400) / <alpha-value>)",
-          200: "rgb(var(--color-red-200) / <alpha-value>)",
-          100: "rgb(var(--color-red-100) / <alpha-value>)",
-        },
-        green: {
-          700: "rgb(var(--color-green-700) / <alpha-value>)",
-          600: "rgb(var(--color-green-600) / <alpha-value>)",
-          500: "rgb(var(--color-green-500) / <alpha-value>)",
-          400: "rgb(var(--color-green-400) / <alpha-value>)",
-          300: "rgb(var(--color-green-300) / <alpha-value>)",
-          200: "rgb(var(--color-green-200) / <alpha-value>)",
-          100: "rgb(var(--color-green-100) / <alpha-value>)",
-        },
-        neutral: {
-          900: "rgb(var(--color-neutral-900) / <alpha-value>)",
-          800: "rgb(var(--color-neutral-800) / <alpha-value>)",
-          700: "rgb(var(--color-neutral-700) / <alpha-value>)",
-          600: "rgb(var(--color-neutral-600) / <alpha-value>)",
-          500: "rgb(var(--color-neutral-500) / <alpha-value>)",
-          400: "rgb(var(--color-neutral-400) / <alpha-value>)",
-          300: "rgb(var(--color-neutral-300) / <alpha-value>)",
-          200: "rgb(var(--color-neutral-200) / <alpha-value>)",
-          100: "rgb(var(--color-neutral-100) / <alpha-value>)",
-          50: "rgb(var(--color-neutral-50) / <alpha-value>)",
-        },
+        // primary: {
+        //   400: "rgb(var(--color-primary-400) / <alpha-value>)",
+        //   500: "rgb(var(--color-primary-500) / <alpha-value>)",
+        //   600: "rgb(var(--color-primary-600) / <alpha-value>)",
+        //   700: "rgb(var(--color-primary-700) / <alpha-value>)",
+        // },
         brand: {
           neutral: "rgb(var(--color-neutral) / <alpha-value>)",
           primary: "rgb(var(--color-primary) / <alpha-value>)",
