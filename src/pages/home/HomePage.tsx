9db5e754807613b54750713c9d9c920c0bba5bb9
happy7yong <happy7yong83@gmail.com>
2026-03-01 00:44:33 +0900

    feat(home-retro): 회고 작성 버튼 기본 비활성화 제어 (#372)

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useHomePlanStore } from "@/entities/day-plan";
+import { useHomePlanStore, useDayPlanReflectionStatusQuery } from "@/entities/day-plan";
 import { RetroWriteStackPage } from "@/pages/retro";
 import { WeeklyReportStackPage } from "@/pages/report";
 import { useStackPage } from "@/widgets/stack";
@@ -14,6 +14,8 @@ export function HomePage({ enabled = true }: HomePageProps) {
   const { push } = useStackPage();
   const homeDate = useHomePlanStore((state) => state.date);
   const dayPlanId = useHomePlanStore((state) => state.dayPlanId);
+  const { data: reflectionStatus, isFetching: isReflectionFetching } =
+    useDayPlanReflectionStatusQuery({ dayPlanId });
 
   const handleOpenPlannerEdit = () => {
     push(<PlannerEditStackPage />);
@@ -44,6 +46,7 @@ export function HomePage({ enabled = true }: HomePageProps) {
             icon="retro"
             label="회고 작성"
             onClick={handleOpenRetroWrite}
+            disabled={reflectionStatus?.alreadyWrote !== false || isReflectionFetching}
           />
           <FloatingActionButton
             icon="edit"
