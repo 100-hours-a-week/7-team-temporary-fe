73e194d03ff8ec0b06f94c8f124026e52dc20e2b
happy7yong <happy7yong83@gmail.com>
2026-03-03 22:07:50 +0900

    refactor(schedule-list): TodoList 엔티티 공통 리스트로 통합

--- src/entities/day-plan-schedule/ui/index.ts
+++ src/entities/day-plan-schedule/ui/index.ts
@@ -3,4 +3,6 @@ export { EditableTaskItem } from "./EditableTaskItem";
 export { ExcludedTaskItem } from "./ExcludedTaskItem";
 export { TaskItemActionRow } from "./TaskItemActionRow";
 export { TodoCartTaskItem } from "./TodoCartTaskItem";
+export { ScheduleTodoList } from "./ScheduleTodoList";
 export type { TodoCartViewMode } from "./TodoCartTaskItem";
+export type { ScheduleTodoListProps, ScheduleTodoListTask } from "./ScheduleTodoList";
